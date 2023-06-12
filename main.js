709b3e8d8e83adf2790d2d7f822781d62e593460
DieuLuong <128762523+lunsiu195@users.noreply.github.com>
2023-06-11 17:56:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+        var typed = new Typed(".auto-input", {
+            strings: ["Web Developer", "UI/UX Designer"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true
+        })
+ 
